7a18b892c06679e023614ddac79e83d5d75b0002
Julian Kosciessa <kosciessa@mpib-berlin.mpg.de>
2022-08-31 12:44:09 +0000

    Initial commit

---
+++ git_init.R
@@ -0,0 +1,6 @@
+# use a function without loading the package
+# package::function
+usethis::use_git_config(
+  user.name = "Julian Kosciessa",
+  user.email = "kosciessa@mpib-berlin.mpg.de",
+  init.defaultBranch = "main") # <-- not necessary but kinder than 'master'
